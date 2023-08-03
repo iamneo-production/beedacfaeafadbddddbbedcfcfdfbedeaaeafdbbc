c943c3238c6f1a03dc8c05cee223c5058f05402f
Iamneo <techadmin@iamneo.ai>
2023-08-03 08:40:37 +0000

    Backup Commit

--- React-Project-Folder/src/App.js
+++ React-Project-Folder/src/App.js
@@ -1,24 +1,19 @@
 import {useState} from 'react';
 import './App.css';
-import Button from "./"
+import Button from "./Components/Button";
+import Card from "./Components/Card";
+import React from 'react';
 
 function App() {
+
+  var qCards;
+  var startBtn;
+  var resultBtn;
+
+  const questionData = []
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="App">
+      
     </div>
   );
 }
